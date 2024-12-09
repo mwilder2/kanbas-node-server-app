ed20ae4c4e19665f26770a19df4d8c0c6f4b4906
mwilder2 <83289313+mwilder2@users.noreply.github.com>
2024-12-08 23:58:12 -0500

    added an env file.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModulesRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
+
+dotenv.config();
 
 const app = express();
 app.use(cors()); // CORS middleware
@@ -19,5 +22,6 @@ EnrollmentRoutes(app);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Access the server at: ${process.env.SERVER_URL || `http://localhost:${PORT}`}`);
 });
